de1fef451967e144bd80e29cc330438144282511
Fran Martinez <fj.martinez.paez@gmail.com>
2021-09-10 17:11:22 +0400

    consume flux

--- src/vue-ui/src/components/HelloWorld.vue
+++ src/vue-ui/src/components/HelloWorld.vue
@@ -34,12 +34,12 @@
 
 
 <script>
-  import controlCenterService from '@/services/ControlCenterService.js'
+  //import controlCenterService from '@/services/ControlCenterService.js'
 
   export default {
     name: 'HelloWorld',
 
-    created() {
+    mounted() {
       this.load()
     },
 
@@ -50,9 +50,33 @@
     methods: {
 
       load() {
-        controlCenterService.environments.getAll().then(it => this.environments = it.data)
-      },
+        
+        let es = new EventSource("http://localhost:8080/api/v1/envs");
 
+        es.addEventListener('message', event => {
+
+          var env = JSON.parse(event.data)
+
+          const index = this.environments.findIndex((e) => e.branch === env.branch);
+
+          if (index === -1) {
+              this.environments.push(env);
+          } else {
+              this.environments[index] = env;
+          }
+
+
+        }, false);
+
+        es.addEventListener('open', event => {
+
+          console.log("eeeee", event);
+
+
+        }, false);
+
+      },
+      
     }
 
   }
